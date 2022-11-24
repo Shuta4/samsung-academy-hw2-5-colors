9a929497018b4b3d668f7f3b17aad3deeb190c0b
Shuta4 <skirdin.nik@gmail.com>
2022-11-24 13:50:18 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Samsung Academy Homework 2-5: Colors
+App with rainbow in different layouts.
---
+++ app/src/main/java/ru/myitschool/lab23/MainActivity.java
@@ -0,0 +1,22 @@
+package ru.myitschool.lab23;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import ru.myitschool.lab23.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        CoordinatorLayout viewRoot = binding.getRoot();
+        setContentView(viewRoot);
+    }
+}
